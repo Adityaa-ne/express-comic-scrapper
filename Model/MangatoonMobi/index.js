a237ce5b9131218d0a22318724e4eef5be2ae768
Agata React 1 <aditya@evetehcsolution.com>
2023-02-17 19:12:50 +0700

    update

--- Model/MangatoonMobi/index.js
+++ Model/MangatoonMobi/index.js
@@ -1,18 +1,33 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const URL = 'https://mangatoon.mobi/id/genre/novel';
-async function getComicList() {
+async function getComicList(num) {
+  const current = num ?? 0;
+  let URL = `https://mangatoon.mobi/id/genre/novel?page=${current}`;
   let list = [];
   try {
     const { data } = await axios.get(URL);
     const $ = cheerio.load(data);
-    const HTML = $('#page-content > div.genre-content > div.items > a > div');
+    const HTML = $('#page-content > div.genre-content > div.items > a');
     HTML.each((a, b) => {
-      let obj = { id: '', title: '', cover: '' };
+      let obj = {
+        id: '',
+        title: '',
+        cover: '',
+        slug: '',
+        detail: '',
+        likes: '',
+      };
       obj.id = a + 1;
       obj.title = $(b).find('div.content-title').text();
       obj.cover = $(b).find('img.lazyload').attr('data-src');
+      obj.slug = $(b)
+        .find('div.content-title')
+        .text()
+        .toLowerCase()
+        .replace(/\s/g, '-');
+      obj.detail = $(b).attr('href');
+      obj.likes = $(b).find('div.list-icon > span:nth-child(2)').text();
       list.push(obj);
     });
   } catch (error) {
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const getMangaList = require('./Model/MangatoonMobi/index');
 /* End module list */
 const app = express();
-const port = process.env.PORT || 3170;
+const port = process.env.PORT || 3000;
 
 /* Express */
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 /* Router */
-app.get('/api/list-comics', async (_, res) => {
+app.get('/api/lists', async (req, res) => {
+  const current = req.query.page;
   try {
-    const data = await getMangaList();
+    const data = await getMangaList(current);
     res.send({
       success: true,
       status: 200,
